3279cf911039f33372a62f893ac03de308511b9d
iqbalnafee <iqbalnafee@gmail.com>
2019-09-21 11:56:27 +0600

    first dart language

---
+++ main.dart
@@ -0,0 +1,16 @@
+import 'package:HelloDartVar/HelloDartVar.dart' as HelloDartVar;
+
+main(List<String> arguments) {
+  var nafees=" last name: iqbal ";
+  int roll;
+  double r;
+  r=1;
+  roll=5;
+  const pi=3.1516;
+  //pi=8.9;
+  var four=4;
+  var two=2;
+  print('Hello world: first name: nafees${nafees.toUpperCase()}  roll: $roll!');
+  print(pi);
+  print("is it true? ${4==2}");
+}
